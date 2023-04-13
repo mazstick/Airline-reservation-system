46477a10d70bd9025107bb9e823233559a4943dd
Mohammad Ali Zahmatkesh <maz.stick1383@gmail.com>
2023-04-13 16:58:35 +0430

    Method class added

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
+    public static Method method = new Method();
     public static Admin admin = new Admin();
     public static Passengers[] passengers = new Passengers[20];
     public static Flights flights = new Flights();
 
     public static void main(String[] args) {
 
-        for (int i = 0; i < 20; i++) {
-            passengers[i] = new Passengers();
-        }
+        method.buildArray(passengers , 20);
         flights.flightLib(flights);
-
-        int counter = 0;
-        while (true) {
-            printMainMenue();
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1:
-                    SignIn signIn = new SignIn(passengers, admin, flights);
-                    break;
-                case 2:
-                    SignUp signUp = new SignUp();
-                    passengers[counter] = new Passengers();
-                    passengers[counter].getUserName();
-                    passengers[counter].getPassword();
-                    counter++;
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + choice);
-            }
-        }
+        method.mainMenu(passengers ,admin, flights);
     }
-
-
-
-
-    public static void printMainMenue() {
-        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
-                "           WELCOME TO AIRELINE RESERVATION SYSTEM\n" +
-                "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
-                "..........................MENU OPTIONS........................\n\n" +
-                "    <1> Sign in\n" +
-                "    <2> Sign up \n");
-    }
-
 }
---
+++ src/Method.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.Scanner;
+
+public class Method {
+    public Scanner scanner = new Scanner(System.in);
+    public void mainMenu(Passengers[] passengers , Admin admin , Flights flights){
+        int counter = 0;
+        while (true) {
+            printLoginMenu();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    SignIn signIn = new SignIn(passengers, admin, flights);
+                    break;
+                case 2:
+                    SignUp signUp = new SignUp();
+                    passengers[counter] = new Passengers();
+                    passengers[counter].getUserName();
+                    passengers[counter].getPassword();
+                    counter++;
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + choice);
+            }
+        }
+    }
+    public void buildArray(Passengers[] passengers , int len){
+        for (int i = 0; i < len; i++) {
+            passengers[i] = new Passengers();
+        }
+    }
+    public void printLoginMenu() {
+        System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+                "           WELCOME TO AIRELINE RESERVATION SYSTEM\n" +
+                "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n" +
+                "..........................MENU OPTIONS........................\n\n" +
+                "    <1> Sign in\n" +
+                "    <2> Sign up \n");
+    }
+}
